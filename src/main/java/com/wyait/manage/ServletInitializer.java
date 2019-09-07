e874031934c24aea23e8f144d2d400fef38c84b9
zhangqiang <heidaodageshiwo@163.com>
2019-09-07 12:07:20 +0800

    springboot layui

---
+++ src/main/java/com/wyait/manage/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.wyait.manage;/*
+package com.wyait.manage;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WyaitManageApplication.class);
+	}
+
+}
+*/
